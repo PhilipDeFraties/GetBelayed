d47b8c312744b395f006f378ec5e33d64332c05b
philipdefraties <philipjfry338@gmail.com>
2021-03-13 20:46:37 -0700

    Adds has_many posts to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  has_many :posts
   validates :first_name, presence: true
   validates :last_name, presence: true
   validates :email, uniqueness: true, presence: true

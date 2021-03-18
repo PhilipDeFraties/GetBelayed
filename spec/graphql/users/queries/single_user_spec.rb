f3da3736e48f435a1b12c4783096b1b8c9953ef4
philipdefraties <philipjfry338@gmail.com>
2021-03-17 21:07:28 -0600

    adds test for single user request

--- spec/graphql/users/queries/single_user_spec.rb
+++ spec/graphql/users/queries/single_user_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe Types::QueryType, type: :request do
   it 'loads single user' do
     query_string = <<~GQL
       query {
-        user {
+        user(id: 4) {
           id
           firstName
           lastName
@@ -16,10 +16,8 @@ RSpec.describe Types::QueryType, type: :request do
       }
     GQL
 
-    users = FactoryBot.create_list(:user, 3)
-
+    user = FactoryBot.create(:user)
     post '/graphql', params: { query: query_string }
-
     expect(response.status).to eq(200)
     json = JSON.parse(response.body, symbolize_names: true)
 
@@ -32,18 +30,18 @@ RSpec.describe Types::QueryType, type: :request do
     expect(json[:data][:user]).to be_a(Hash)
     expect(json[:data][:user]).to have_key(:id)
     expect(json[:data][:user][:id]).to be_a(String)
-    expect(json[:data][:user][:id].to_i).to eq(users[index].id)
+    expect(json[:data][:user][:id].to_i).to eq(user.id)
     expect(json[:data][:user]).to have_key(:firstName)
     expect(json[:data][:user][:firstName]).to be_a(String)
-    expect(json[:data][:user][:firstName]).to eq(users[index].first_name)
+    expect(json[:data][:user][:firstName]).to eq(user.first_name)
     expect(json[:data][:user]).to have_key(:lastName)
     expect(json[:data][:user][:lastName]).to be_a(String)
-    expect(json[:data][:user][:lastName]).to eq(users[index].last_name)
+    expect(json[:data][:user][:lastName]).to eq(user.last_name)
     expect(json[:data][:user]).to have_key(:email)
     expect(json[:data][:user][:email]).to be_a(String)
-    expect(json[:data][:user][:email]).to eq(users[index].email)
+    expect(json[:data][:user][:email]).to eq(user.email)
     expect(json[:data][:user]).to have_key(:gender)
     expect(json[:data][:user][:gender]).to be_a(String)
-    expect(json[:data][:user][:gender]).to eq(users[index].gender)
+    expect(json[:data][:user][:gender]).to eq(user.gender)
   end
 end

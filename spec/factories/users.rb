aa46d4b15d1f958560169f67128187aab203b425
PhilipDeFraties <philipjfry338@gmail.com>
2021-03-22 23:41:23 -0600

    removes gender from user factory

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -5,6 +5,5 @@ FactoryBot.define do
     first_name { Faker::Name.unique.first_name }
     last_name { Faker::Name.unique.last_name }
     email { Faker::Internet.email }
-    gender { Faker::Gender.binary_type }
   end
 end

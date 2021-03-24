086c1a21791e6ccfa50503c075b641ae40b16886
PhilipDeFraties <philipjfry338@gmail.com>
2021-03-24 14:55:07 -0600

    Adds descriptions to user query types, reorganizes blocks

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -9,18 +9,19 @@ module Types
     # Add root-level fields here.
     # They will be entry points for queries on your schema.
 
-    field :users,
-          [Types::UserType],
-          null: false,
-          description: 'Returns a list of users'
+    field :users, [Types::UserType], null: false do
+      description 'Returns a list of users'
+    end
+
+    field :user, Types::UserType, null: false do
+      description 'Find a user by ID'
+      argument :id, ID, required: true
+    end
+
     def users
       User.all
     end
 
-    field :user, Types::UserType, null: false do
-      argument :id, ID, required: true
-    end
-    
     def user(id:)
       User.find(id)
     end
